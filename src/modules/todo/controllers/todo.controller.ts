1407e4cf442caa6819f129e3fdde0180ab9825a7
cxvint <cxvint@gmail.com>
2021-11-29 01:24:12 +0300

    Conected DB to controller and implemented simple CRUD in Nest

--- src/modules/todo/controllers/todo.controller.ts
+++ src/modules/todo/controllers/todo.controller.ts
@@ -1,35 +1,55 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Todo } from '../entitiles/todo.entity';
+import { BadRequestException, Body, Controller, Delete, Get, HttpException, HttpStatus, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../entities/todo.entity';
 import { CreateDto, UpdateDto } from './dto';
-
 @Controller('rest/todo')
 export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
 
   @Get()
-  getAllAction(): string {
-    return "Todo Get All";
+  getAllAction(): Promise<Todo[]> {
+    return this.todoService.findAll();
   }
 
   @Get(':id')
-  getOneAction(@Param('id') id: string): string {
-    return "Todo Get One by id=" + id ;
-  }
+  async getOneAction(@Param('id') id: string): Promise<Todo> {
+    const todo = this.todoService.findOne(id);
+    if (todo === undefined) {
+      throw new HttpException(
+        'Todo with id=' + id + 'not exists',
+        HttpStatus.NOT_FOUND
+        );
+    }
+    return todo;
+  } 
 
   @Post()
-  saveAction(@Body() todo: CreateDto): CreateDto {
-    return todo;
+  createAction(@Body() createDto: CreateDto): Promise<Todo> {
+    const todo = new Todo();
+    todo.title = createDto.title;
+    if (createDto.isComplited !== undefined) {
+      todo.isComplited = createDto.isComplited;
+    }
+    return this.todoService.create(todo);
   }
 
   @Put(':id')
-  updateAction(
-    @Param('id') id: string, 
-    @Body() todo: UpdateDto
-  ): UpdateDto {
-    return todo;
+  async updateAction(
+    @Param('id') id: string,
+    @Body() {title, isComplited = false}: UpdateDto
+  ): Promise<Todo> {
+    const todo = await this.todoService.findOne(id);
+    if (todo === undefined) {
+      throw new NotFoundException('Todo with id=' + id + 'not exists');
+    }
+    todo.title = id;
+    todo.title = title;
+    todo.isComplited = isComplited;
+    return this.todoService.update(todo);
   }
 
   @Delete(':id')
-  deleteAction(@Param('id') id: string): string {
-    return "Delete Todo by id=" + id;
+  deleteAction(@Param('id') id: string): Promise<void> {
+    return this.todoService.remove(id);
   }
 }
